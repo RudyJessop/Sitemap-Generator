da814958e204e4a7dbe57b5150d63d3643e14d76
Rudy Jessop <rudy@pcjunkii.com>
2014-07-21 13:05:13 -0400

    MIT license update
    MIT license update and some sitemap directory url update

--- sitemapgenerator.php
+++ sitemapgenerator.php
@@ -1,10 +1,10 @@
 <?php
-	echo '<!-- /* This Sitemap generator was built by Rudy Jessop at http://rudyjessop.com. 
+	echo '<!-- /* This Sitemap generator was built by Rudy Jessop at http://rudyjessop.com 
 	This software is under MIT Commerical Licence Copyright &copy; 2014 Rudy Jessop Creative Professional */-->'
 
 	echo '<!-- /* 
 Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
+of this software and associated documentation files (Site Map Generator), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
@@ -29,14 +29,16 @@ THE SOFTWARE.
 				xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
 				http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' .PHP_EOL;
 
-			define('SITEMAP_DIR','SITEMAP_DIR_URL');
-			$frequently = "always";
+			define('SITEMAP_DIR','./');
+			define('SITEMAP_DIR_URL', 'http://www.example.com/')
+
+			$freq = "always";
 			$prio = 1;
 
 	function urlElement($url){
 		echo '<url>\n
 				<loc>'.$url.'<loc>\n
-				<changefreq>'.$frequently.'</changefreq>\n
+				<changefreq>'.$freq.'</changefreq>\n
 				<priority>'.$prio.'</priority>\n
 			</url>'.PHP_EOL;
 
@@ -45,5 +47,8 @@ THE SOFTWARE.
 	echo '</urlset>'
 
 	urlElement("http://rudyjessop.com");
-	urlElement("http://github.com/RudyJessop/Sitemapgenerator");
+	urlElement("http://github.com/RudyJessop/Sitemap-Generator");
+	urlElement("http://twitter.com/rudy_di_lover");
+
+
 ?>
